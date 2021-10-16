02867c626240501df3f1847ef4d63931e1ebcdbb
dougfort <doug.fort@gmail.com>
2021-10-16 14:36:54 -0400

    use constants for magic numbers

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use futures::{stream, StreamExt};
 use rand::{thread_rng, Rng};
 use std::time::{Duration};
 
+const UPPER_BOUND: u64 = 200;
+const INSTANCE_COUNT: usize = 20;
+const SLEEP_UPPER_BOUND: u64 = 20;
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    stream::iter(0..200u64)
-        .for_each_concurrent(20, |number| async move {
+    stream::iter(0..UPPER_BOUND)
+        .for_each_concurrent(INSTANCE_COUNT, |number| async move {
             let mut rng = thread_rng();
-            let sleep_ms: u64 = rng.gen_range(0..20);
+            let sleep_ms: u64 = rng.gen_range(0..SLEEP_UPPER_BOUND);
             tokio::time::sleep(Duration::from_millis(sleep_ms)).await;
             println!("{}", number);
         })
