c339d994db2d4839d693e7ef976822d0a0a146f4
dougfort <doug.fort@gmail.com>
2021-10-19 08:49:52 -0400

    spawn a stub mutator

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,29 @@ use axum::{
     Router,
 };
 use anyhow::Error;
+use rand::Rng;
+use std::time::{Duration};
+use tokio::sync::Notify;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let handle = tokio::spawn(async move {
+    let mutator_notify = Arc::new(Notify::new());
+    let mutator_notify2 = mutator_notify.clone();
 
+    let handle = tokio::spawn(async move {
+        let mut more = true;
+        while more {
+            let sleep_interval = {
+                let mut rng = rand::thread_rng();
+                Duration::from_secs(rng.gen_range(0..20))
+            };
+            println!("sleep: {:?}", sleep_interval);    
+            tokio::select! {
+                _ = tokio::time::sleep(sleep_interval) => {}
+                _ = mutator_notify2.notified() => {more = false}
+            }            
+        }
     });
 
     // build our application with a single route
@@ -19,6 +37,7 @@ async fn main() -> Result<(), Error> {
         .with_graceful_shutdown(shutdown_signal())
         .await?;
 
+        mutator_notify.notify_one();
     let join_result = handle.await?;
     println!("join result = {:?}", join_result);
  
